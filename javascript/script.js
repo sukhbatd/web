ee96c08c9f2d32fd8b8ef6bd70997ddc42a6b1ba
ariuka-se <96542615+ariuka-se@users.noreply.github.com>
2023-04-24 11:35:00 +0800

    ariuka

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,10 @@
-$(document).ready(function(e){
-    object.products.forEach((i) => loadProducts(i));
-});
-function loadProducts(data){
-    var li = document.createElement('li');
-    li.innerHTML =  `<article class="product" id="${data.id}">
-                        <img src="${ data.img } " class="image"/>
-                        
-                            <h3 class="title" contenteditable="true" id="title${data.id}">${ data.name } </h3>
-                            <h3 class="price" contenteditable="true" id="title${data.id}">${ data.price} </h3>
-                      
-                    </article>`
-    document.getElementById("productList").appendChild(li);
-}
+fetch('../javascript/product.json', {
+  method: 'GET',
+  headers: {
+    'Authorization': 'Bearer TOKEN',
+    'Content-Type': 'application/json'
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(product))
+  .catch(error => console.error("Error to us"));
