cebb02b3f3c15204bd940c1bd3dbe0ed05594fcb
“sukhee” <d.suhbat2000@gmail.com>
2023-04-29 11:52:38 +0800

    json

---
+++ module/footer.js
@@ -0,0 +1,40 @@
+export function Footer() {
+  return (
+    <>
+      <div class="row">
+        <div class="column">
+          <i class="bx bxs-store"></i>
+          <span class="serve">Үйлчилгээний нөхцөл</span>
+          <p>
+            Загвар болон өдөр өөрчлөх боломжгүй байгаа тул та сайн нягталж
+            шалгаад захиалаарай. Бялуу болон тартны хүргэлтийн үйлчилгээг
+            "Улаанбаатар такси" компани хариуцаж байгаа та хүргэлттэй холбоотой
+            асуух лавлах зүйл холбоо барина уу.
+          </p>
+        </div>
+        <div class="column">
+          <h3>Холбоо барих</h3>
+          <i class="bx bxs-phone-call"></i>
+          <span class="call">976-9999-9999</span>
+          <address>
+            <a href="">
+              {" "}
+              <i class="bx bxl-facebook-circle"></i>
+            </a>
+            <a href="">
+              <i class="bx bxl-gmail"></i>
+            </a>
+            <a href="">
+              <i class="bx bxl-instagram-alt"></i>
+            </a>
+            <a href="">
+              <i class="bx bxs-map"></i>
+            </a>
+          </address>
+          {/* <a href="mobile.html"><img src="../image/gplay.svg" alt="Google Play store link"></a>
+                         <a href=""><img src="./image/appstore.svg" alt=""></a> */}
+        </div>
+      </div>
+    </>
+  );
+}
